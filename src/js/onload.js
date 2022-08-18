e18ba7e59f3bf8b25fea35b1bd7fa26d5e78bd91
Algebra-FUN <worldcreator@foxmail.com>
2022-08-18 09:13:16 +0800

    Add self-edit function

--- src/js/onload.js
+++ src/js/onload.js
@@ -1,13 +1,26 @@
 function setDate(){
-    now = new Date()
-    hour = now.getHours()
-    minute = now.getMinutes()
-    dateStr = new Date().toLocaleString('default', { month: 'short', day: 'numeric' })
-    timeStr = 100*hour+minute
-    renderDate = document.querySelector('.render-date')
+    var now = new Date()
+    var hour = now.getHours()
+    var minute = now.getMinutes()
+    var dateStr = new Date().toLocaleString('default', { month: 'short', day: 'numeric' })
+    var timeStr = 100*hour+minute
+    var renderDate = document.querySelector('.render-date')
     renderDate.innerHTML = `${dateStr} <br>${timeStr}`
+}
+
+function clickUploadImage(){
+    var photo = document.querySelector('.photo')
+    var fileUpload = document.querySelector('.photo > input')
+    var img = document.querySelector('.photo > img')
+    photo.onclick = function(){
+        fileUpload.click()
+    }
+    fileUpload.onchange = function(e){
+        img.src = URL.createObjectURL(e.target.files[0])
+    }
 }
 
 window.onload = function(){
     setDate()
+    clickUploadImage()
 }
